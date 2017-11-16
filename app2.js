37295c3b22d1de92f071e565d702ec6220edb9ca
SuperValou96 <32134811+SuperValou96@users.noreply.github.com>
2017-11-16 12:07:37 +0100

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,29 @@
+const heroku_url_api = "https://mighty-plains-77473.herokuapp.com/api/rooms/On";
+// On retourne la liste de lumière on
+
+const vm = new Vue({
+    el: '#app2',
+    data: {
+        rooms: [],
+        selectedRoom: 0
+    },
+    mounted() {
+        axios.get(heroku_url_api)
+            .then(response => {this.rooms = response.data});
+    },
+    methods: {
+        switchLight(room) {
+            this.selectedRoom = room;
+            let post_url = heroku_url_api + "/" + room.id + "/switchLightl";
+            axios.post(post_url, {roomId: room.id})
+                .then(response => {this.rooms = response.data});
+            },
+        switchRinger(room) {
+            this.selectedRoom = room;
+            let post_url = heroku_url_api + "/" + room.id + "/switchRingerl";
+            axios.post(post_url, {roomId: room.id})
+                .then(response => {this.rooms = response.data});
+        },
+
+    }
+});
